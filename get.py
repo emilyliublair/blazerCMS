3a32659cc30602fd6e6e028fa4247a150364c9a5
M10T <milantenn@gmail.com>
2020-07-31 16:56:43 -0400

    Added announcements

--- get.py
+++ get.py
@@ -17,14 +17,26 @@ def events(lang):
     else:
         return send_file('data/error.json')
 
+@get_info.route('/<lang>/announcements')
+def announcements(lang):
+    start = int(request.args.get('start', 0))
+    end = request.args.get('end','end')
+    if end != 'end':
+        end = int(end)
+    if(os.path.isdir('data/'+lang)):
+        r = make_response(from_log('data/'+lang+'/announcements', start, end))
+        r.mimetype='application/json'
+        return r
+    else:
+        return send_file('data/error.json')
+    
 
 def from_log(folder,start,end):
-    print(start,end)
     if start < 0 or (end != 'end' and end < 0):
         raise ValueError
     with open(folder+'/log.json') as f:
         names = json.load(f)['names']
-        collection = '{"data":[%s]}'
+        collection = '{"data":[%s],"error":false}'
         items = []
         for n in names[start:(len(names) if end=='end' else end)]:
             with open(folder+'/'+n) as e:
