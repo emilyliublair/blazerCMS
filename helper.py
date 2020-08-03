22d0ccddbe77b583e1d417aea5eee672172f4c8e
M10T <milantenn@gmail.com>
2020-08-02 20:45:27 -0400

    Added login redirects

--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
 import json
+from functools import wraps
+from flask import session,redirect,url_for
 
 def from_log(folder,start,end):
     if start < 0 or (end != 'end' and end < 0):
@@ -16,3 +18,14 @@ def update_log(folder,fileName):
     with open(folder+'/log.json') as f:
         names = json.load(f)['names']
         json.dump({'names':[fileName]+names},open(folder+'/log.json','w'))
+
+def sessionvalidated(f):
+    @wraps(f)
+    def wrapper(*args,**kwargs):
+        if "loggedIn" not in session:
+            return redirect(url_for("ui.login"))
+        elif session["loggedIn"] == False:
+            return redirect(url_for("ui.login"))
+        else:
+            return f(*args,**kwargs)
+    return wrapper
--- shindel.py
+++ shindel.py
@@ -1,7 +1,7 @@
 import os
 from flask import Blueprint, send_file, make_response, request, session, render_template, redirect, url_for
 import json
-from helper import from_log,update_log
+from helper import from_log,update_log,sessionvalidated
 import json
 
 ui = Blueprint('ui', __name__, url_prefix='/ui')
@@ -23,51 +23,38 @@ def login():
         else:
             return render_template("login.html")
 
-
 @ui.route('/')
-def home():
-    if "loggedIn" not in session:
-        return "You are not logged in"
-    elif session["loggedIn"] == False:
-        return "You are not logged in"
-    else: 
-        return render_template("main.html")
+@sessionvalidated
+def home():    
+    return render_template("main.html")
 
 @ui.route('/<lang>/announcements',methods=["GET","POST"])
+@sessionvalidated
 def announcements(lang):
-    if 'loggedIn' not in session:
-        return "You are not logged in"
-    elif session["loggedIn"] == False:
-        return "You are not logged in"
-    else:
-        announcelog=json.loads(from_log('data/'+lang+'/announcements',0,'end'))
-        names = [x['message'] for x in announcelog['data']]
-        if request.method == "GET":
-            return render_template('announcements.html',names=names)
-        elif request.method == "POST":
-            name = 'announcement'+str(len(announcelog['data']))+'.json'
-            content = json.dumps(dict(request.form))
-            with open('data/'+lang+'/announcements/'+name,'w') as f:
-                f.write(content)
-            names.insert(0,request.form['message'])
-            update_log('data/'+lang+'/announcements',name)
-            return render_template('announcements.html',names=names)
+    announcelog=json.loads(from_log('data/'+lang+'/announcements',0,'end'))
+    names = [x['message'] for x in announcelog['data']]
+    if request.method == "GET":
+        return render_template('announcements.html',names=names)
+    elif request.method == "POST":
+        name = 'announcement'+str(len(announcelog['data']))+'.json'
+        content = json.dumps(dict(request.form))
+        with open('data/'+lang+'/announcements/'+name,'w') as f:
+            f.write(content)
+        names.insert(0,request.form['message'])
+        update_log('data/'+lang+'/announcements',name)
+        return render_template('announcements.html',names=names)
 
 @ui.route('/<lang>/events',methods=["GET","POST"])
+@sessionvalidated
 def events(lang):
-    if 'loggedIn' not in session:
-        return "You are not logged in"
-    elif session["loggedIn"] == False:
-        return "You are not logged in"
-    else:
-        events=json.loads(from_log('data/'+lang+'/events',0,'end'))['data']
-        if request.method == "GET":
-            return render_template('events.html',events=events)
-        elif request.method == "POST":
-            name = 'event'+str(len(events))+'.json'
-            content = json.dumps(dict(request.form))
-            with open('data/'+lang+'/events/'+name,'w') as f:
-                f.write(content)
-            events.insert(0,dict(request.form))
-            update_log('data/'+lang+'/events',name)
-            return render_template('events.html',events=events)
+    events=json.loads(from_log('data/'+lang+'/events',0,'end'))['data']
+    if request.method == "GET":
+        return render_template('events.html',events=events)
+    elif request.method == "POST":
+        name = 'event'+str(len(events))+'.json'
+        content = json.dumps(dict(request.form))
+        with open('data/'+lang+'/events/'+name,'w') as f:
+            f.write(content)
+        events.insert(0,dict(request.form))
+        update_log('data/'+lang+'/events',name)
+        return render_template('events.html',events=events)
